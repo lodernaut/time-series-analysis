77ea4715c9a4937715ac7a995cbfc3233ed9c742
Gabriel Lemos <gabriellemosdev@gmail.com>
2024-10-10 16:11:41 -0300

    testing git commit

--- time-series.py
+++ time-series.py
@@ -10,12 +10,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import tensorflow as tf
+
+# import tensorflow as tf
 
 # ajusta o tamanho padrão das figuras(gráficos) gerados.
-mpl.rcParams["figure.figsize"] = (8, 6)
+# mpl.rcParams["figure.figsize"] = (8, 6)
 # desativa a exibição da grade (grid) nos eixos dos gráficos.
-mpl.rcParams["axes.grid"] = False
+# mpl.rcParams["axes.grid"] = False
 
 # previsões por hora, subamostrar os dados de intervalos de 10 minutos para intervalos de uma hora:
-df = pd.read_csv("csv_path")
+# df = pd.read_csv("csv_path")
+
+print("testing git commit")
